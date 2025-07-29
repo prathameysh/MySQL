72d7f65154c1c94ff37eb134c2b592851330c8fd
Prathamesh Gavatre <prathameshgavatre@gmail.com>
2025-07-29 18:43:46 +0530

    changes

--- MySQL local.sql
+++ MySQL local.sql
@@ -59,3 +59,47 @@ INSERT INTO Worker
 (008, 'Geetika', 'Chauhan', 90000, '2014-04-11 09:00:00', 'Admin');
 
 SELECT * FROM Worker;
+
+CREATE TABLE Bonus (
+    WORKER_REF_ID INT,
+    BONUS_AMOUNT INT(10),
+    BONUS_DATE DATETIME,
+    FOREIGN KEY (WORKER_REF_ID)
+        REFERENCES Worker(WORKER_ID)
+        ON DELETE CASCADE
+);
+
+INSERT INTO Bonus
+(WORKER_REF_ID, BONUS_AMOUNT, BONUS_DATE) VALUES
+(001, 5000, '2016-02-20'),
+(002, 3000, '2016-06-11'),
+(003, 4000, '2016-02-20'),
+(001, 4500, '2016-02-20'),
+(002, 3500, '2016-06-11');
+
+CREATE TABLE Title (
+    WORKER_REF_ID INT,
+    WORKER_TITLE CHAR(25),
+    AFFECTED_FROM DATETIME,
+    FOREIGN KEY (WORKER_REF_ID)
+        REFERENCES Worker(WORKER_ID)
+        ON DELETE CASCADE
+);
+
+INSERT INTO Title
+(WORKER_REF_ID, WORKER_TITLE, AFFECTED_FROM) VALUES
+(001, 'Manager', '2016-02-20 00:00:00'),
+(002, 'Executive', '2016-06-11 00:00:00'),
+(008, 'Executive', '2016-06-11 00:00:00'),
+(005, 'Manager', '2016-06-11 00:00:00'),
+(004, 'Asst. Manager', '2016-06-11 00:00:00'),
+(007, 'Executive', '2016-06-11 00:00:00'),
+(006, 'Lead', '2016-06-11 00:00:00'),
+(003, 'Lead', '2016-06-11 00:00:00');
+
+select * 
+from worker 
+order by salary desc LIMIT 1,3;
+
+select w1.* from worker w1, worker w2 where w1.salary = w2.salary and w1.worker_id !=
+w2.worker_id;
